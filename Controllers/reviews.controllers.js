1fdff081952f6212cc89064499510ed664bafda6
Ryan Lee <ryanlee@ryans-imac.home>
2022-09-06 14:52:17 +0100

    complete ticket 6

--- Controllers/reviews.controllers.js
+++ Controllers/reviews.controllers.js
@@ -1,4 +1,4 @@
-const { selectReview } = require("../models/reviews.models");
+const { selectReview, updateReviewVotes } = require("../models/reviews.models");
 
 exports.getReview = (req, res, next) => {
   const id = req.params.review_id;
@@ -11,6 +11,24 @@ exports.getReview = (req, res, next) => {
         next({
           status: 400,
           message: "Invalid id provided, a review id must be a number.",
+        });
+      }
+      next(err);
+    });
+};
+
+exports.patchReviewVotes = (req, res, next) => {
+  const voteChange = req.body.inc_votes;
+  const id = req.params.review_id;
+  updateReviewVotes(voteChange, id)
+    .then(([review]) => {
+      res.status(201).send({ review });
+    })
+    .catch((err) => {
+      if (err.code === "22P02") {
+        next({
+          status: 400,
+          message: "Invalid id or vote count provided, these must be a number.",
         });
       }
       next(err);
