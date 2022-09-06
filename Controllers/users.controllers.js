1c38598ce4fadcf28a23e3376c06659dbe64c9be
Ryan Lee <ryanlee@ryans-imac.home>
2022-09-06 12:52:16 +0100

    compelete ticket5

---
+++ Controllers/users.controllers.js
@@ -0,0 +1,9 @@
+const { selectUsers } = require("../models/users.models")
+
+
+exports.getUsers = (req, res) => {
+    selectUsers()
+    .then((users) => {
+    res.status(200).send({users: users})
+    })
+    }
